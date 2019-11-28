fc4a09880a3962689193f367dc57a4d496e0ffa0
ziurregor <rruiz_1@hotmail.com>
2019-11-28 18:20:08 -0400

    Adciones al proyecto

---
+++ REST_API_Consola/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    
+    using System.IO;
+    using System.Linq;
+using REST_APi.DataAccess;
+using REST_APi.Model;
+
+namespace REST_API_Consola
+{
+    class Program
+    {
+        public static void Main()
+        {
+            using (var db = new EntityFrameworkSQLite())
+            {
+                ICollection<Users> users = new Collection<Users>();
+
+                db.User.Add(new Users
+                {
+                    id = 1,
+                    name = "Roger Ruiz",
+                    active = true
+                });
+
+                var count = db.SaveChanges();
+
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.User)
+                {
+                    Console.WriteLine(" - {0}", blog.name);
+                }
+            }
+        }
+    }
+}
---
+++ REST_APi/Model/Tareas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace REST_APi.Model
+{
+    public class Tareas
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    
+    }
+}
+
