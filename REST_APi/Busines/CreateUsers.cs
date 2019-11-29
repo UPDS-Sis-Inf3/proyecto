77325c89209b21a952bbecf4a19ae28cf236da97
persa40 <lp.kebel.vargas.a@upds.net.bo>
2019-11-28 20:11:59 -0400

    se modifico el modelo

---
+++ REST_APi/Busines/CreateUsers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using REST_APi.Model;
+using REST_APi.DataAccess;
+//using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace REST_APi.Busines
+{
+    public class BUsers
+    {
+        public void CreateUsers(Users users)
+        {
+            using (var db = new EntityFrameworkSQLite())
+            {
+               // ICollection<Users> users1 = new Collection<Users>();
+
+                db.User.Add(users);
+                ///db.User.Add(users);
+                var count = db.SaveChanges();
+
+                //Console.WriteLine("{0} records saved to database", count);
+
+                //Console.WriteLine();
+                
+            }
+        }
+
+
+
+
+    }
+}
---
+++ REST_APi/DataAccess/EntityFrameworkSQLite.cs
@@ -0,0 +1,19 @@
+﻿namespace REST_APi.DataAccess
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+    using REST_APi.Model;
+
+
+    public class EntityFrameworkSQLite : DbContext
+    {
+        public DbSet<Users> User { get; set; }
+        public DbSet<Tareas> Tarea { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=Tareas.db");
+        }
+
+    }
+}
